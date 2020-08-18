a93a517c94e90d32a1e7a8e1846b98a843fa3e0e
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-08-17 18:47:54 -0700

    Set up separate file for email and pass

--- scraper.py
+++ scraper.py
@@ -1,13 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
+import smtplib
+import time
+import config
 
 URL = ""
 
-headers = {"User-Agent": Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/
-			537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36}
+headers = {"User-Agent": {config.user_agent}}
 
-page = requests.get(URL, headers=headers)
 
-soup = BeautifulSoup(page.content, 'html.parser')
+def check_headlines():
+	page = requests.get(URL, headers=headers)
 
-title = soup.find(id="").get_text()
+	soup = BeautifulSoup(page.content, 'html.parser')
+
+	title = soup.find(id="").get_text()
+
+
+def send_email():
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.ehlo()
+	server.starttls()
+	server.ehlo()
+
+	server.login({config.email}, {config.password})
+
+	subject = ""
+	body = ""
+	msg = f"Subject: {subject} \n\n {body}"
+
+	server.sendmail(
+			{config.email},
+			{config.email},
+			msg
+		)
+	server.quit()
+
+while True:
+	check_headlines()
+	time.sleep(86400)
